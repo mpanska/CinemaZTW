70b0df2bef11e49f506fad49d1809adfc77b857b
oazaboy <22818302+oazaboy@users.noreply.github.com>
2021-05-23 13:35:45 +0200

    add ticket controller

---
+++ src/main/java/com/ztw/cinema/controller/TicketController.java
@@ -0,0 +1,27 @@
+package com.ztw.cinema.controller;
+
+import com.ztw.cinema.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tickets")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    @Autowired
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getMovie(@PathVariable Long id) {
+        return new ResponseEntity<>(ticketService.findById(id), HttpStatus.OK);
+    }
+}
